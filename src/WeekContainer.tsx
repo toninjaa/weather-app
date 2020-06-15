625049453288f5210b0854c78ac43f6b79d33e13
toninjaa <tonjagod@gmail.com>
2020-06-15 11:19:22 -0400

    connect to weather api and successfully retrieve data

--- src/WeekContainer.tsx
+++ src/WeekContainer.tsx
@@ -1,6 +1,22 @@
 import * as React from 'react';
+import apiConfig from './apiKeys';
+
+const { useEffect } = React;
 
 function WeekContainer() {
+  const weatherKey = apiConfig.openWeatherMapKey;
+  const weatherURL = `http://api.openweathermap.org/data/2.5/forecast?zip=08817&units=imperial&APPID=${weatherKey}`;
+
+  useEffect(() => {
+    fetch(weatherURL).then((res) => {
+      return res.json();
+    }).then((res) => {
+      console.log('data', res);
+    }).catch((rej) => {
+      console.log('weather fetch failed', rej);
+    })
+  }, [])
+
   return (
     <>
       <h1>Week Container</h1>
