238d43977b1b384d39c3326f9fa78b117d4d0604
toninjaa <tonjagod@gmail.com>
2020-06-15 12:06:37 -0400

    filter results into state objects

--- src/WeekContainer.tsx
+++ src/WeekContainer.tsx
@@ -1,21 +1,42 @@
 import * as React from 'react';
 import apiConfig from './apiKeys';
 
-const { useEffect } = React;
+const { useEffect, useState } = React;
 
 function WeekContainer() {
   const weatherKey = apiConfig.openWeatherMapKey;
   const weatherURL = `http://api.openweathermap.org/data/2.5/forecast?zip=08817&units=imperial&APPID=${weatherKey}`;
 
+  const [weather, setWeather] = useState({
+    fullWeather: {},
+    dailyWeather: {},
+    error: false,
+  });
+
   useEffect(() => {
     fetch(weatherURL).then((res) => {
       return res.json();
     }).then((res) => {
-      console.log('data', res);
+      console.log('res', res);
+      const daily = res.list.filter((day: any) => {
+        return day.dt_txt.includes('18:00:00')
+      });
+      setWeather({
+        ...weather,
+        fullWeather: res.list,
+        dailyWeather: daily,
+      });
+
     }).catch((rej) => {
       console.log('weather fetch failed', rej);
+      setWeather({
+        ...weather,
+        error: true,
+      });
     })
   }, [])
+
+  console.log('weather', weather);
 
   return (
     <>
