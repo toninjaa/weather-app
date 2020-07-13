096b0fe54d060ef16a37287eef67bbdb034a917e
toninjaa <tonjagod@gmail.com>
2020-07-13 18:01:50 -0400

    save for night - working on detailedforecast

--- src/components/DayContainer.tsx
+++ src/components/DayContainer.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import { Button } from '@material-ui/core';
+import DetailedDayContainer from './DetailedDayContainer';
 
 interface Props {
   dayData: any,
@@ -11,27 +13,60 @@ function DayContainer(props: Props) {
   // TODO: update readme
   // TODO: improve UX
   // TODO: add date number to day header
+  // TODO: can add onclick to expand to detailedForecast and also hourly
+  console.log(dayData);
+  
+  function getMonthandDay(dayWeather: any) {
+    const d = new Date(dayWeather.startTime);
+    const day = d.getDate();
+    const monthNum = d.getMonth();
+    const monthsInYear = [
+      'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'Spetember', 'October', 'November', 'December'
+    ];
+    const monthName = monthsInYear[monthNum];
+    const date = `${monthName} ${day}`;
+    return date;
+  }
 
-  // TODO: can add onclick to expand detailedForecast
+  function handleDayClick(dayData: any) {
+    return (
+      <DetailedDayContainer dayData={dayData} />
+    )
+  }
+
   return (
     <>
       {dayData.map((d: any, i: number) => (
         <div key={i} className="Week-item">
-          <h2 className="Day-header">{d.name}</h2>
+          <h2 className="Day-header">
+            {d.name}
+            <br/>
+            {getMonthandDay(d)}
+          </h2>
           <img src={d.icon} alt="weather icon" />
           <h3>{d.shortForecast}</h3>
           <h4 className="Day-item">Temp: {d.temperature}°F</h4>
           <h4>Wind Speed: {d.windSpeed}</h4>
+          <Button onClick={handleDayClick}>
+            Detailed Forecast
+          </Button>
         </div>
       ))}
 
       {nightData.map((n: any, i: number) => (
       <div key={i} className="Week-item">
-        <h2 className="Day-header">{n.name}</h2>
+        <h2 className="Day-header">
+          {n.name}
+          <br />
+          {getMonthandDay(n)}
+        </h2>
         <img src={n.icon} alt="weather icon" />
         <h4 className="Day-item">{n.shortForecast}</h4>
         <h4 className="Day-item">Temp: {n.temperature}°F</h4>
         <h4>Wind Speed: {n.windSpeed}</h4>
+        <Button onClick={handleDayClick}>
+          Detailed Forecast
+        </Button>
       </div>
       ))}
     </>
---
+++ src/components/DetailedDayContainer.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+interface Props {
+  dayData: any,
+}
+
+export default function DetailedDayContainer(props: Props){
+  const { dayData } = props;
+
+  return (
+    <>
+      <h1>Detailed Day Beta</h1>
+    </>
+  )
+}
