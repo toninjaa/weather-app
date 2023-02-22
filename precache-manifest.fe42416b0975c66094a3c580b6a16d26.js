c0b1019da6bb2621ada7690cf82a7ff09e3c23bb
toninjaa <tonjagod@gmail.com>
2023-02-22 12:30:41 -0500

    Updates

---
+++ precache-manifest.fe42416b0975c66094a3c580b6a16d26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5457da69d6077c7dc5629019b93cba16",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "919e633d70d8357d2f4d",
+    "url": "/weather-app/static/css/main.bb97d7ac.chunk.css"
+  },
+  {
+    "revision": "3729a623d6b8ceb50429",
+    "url": "/weather-app/static/js/2.1e2ae7ac.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/weather-app/static/js/2.1e2ae7ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "919e633d70d8357d2f4d",
+    "url": "/weather-app/static/js/main.5db7b539.chunk.js"
+  },
+  {
+    "revision": "4d98715402eae1cd268b",
+    "url": "/weather-app/static/js/runtime-main.67a1ea2a.js"
+  }
+]);
